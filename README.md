9b969a73da25cacd9196265c067ab75a0ed8792f
Eduard Anton <eduard.anton2002@gmail.com>
2021-05-09 13:08:05 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# NewsFeed Express Server
+
+
+
+## Running
+
+Make sure to add a `config.js` file in the `config` folder. See the example there for more details.
+
+Production mode:
+
+```shell
+npm start
+```
+
+Development (Webpack dev server) mode:
+
+```shell
+npm run start:dev
+```
---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db: 'mongodb+srv://ToHacks_User:ToHacks123@mapiccluster.sv9p0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    db_dev: 'mongodb+srv://ToHacks_User:ToHacks123@mapiccluster.sv9p0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
