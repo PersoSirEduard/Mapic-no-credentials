4c4353fa1d33d6f10bff56edda81aab7492cfdbf
Eduard Anton <eduard.anton2002@gmail.com>
2021-05-11 11:18:21 -0400

    Delete config.js

--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    db: 'mongodb+srv://ToHacks_User:ToHacks123@mapiccluster.sv9p0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    db_dev: 'mongodb+srv://ToHacks_User:ToHacks123@mapiccluster.sv9p0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
